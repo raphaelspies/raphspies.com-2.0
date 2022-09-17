dbd299d1e6228931b491a8046d33157810fc2ba7
raphspies <raphspies@gmail.com>
2022-09-17 00:22:10 -0700

    updated package.json project info

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,12 +6,16 @@ export default function Projects() {
       <h1>Projects</h1>
 
       <h2>Snome</h2>
+      A collaborative effort to build a snow-home swapping and social media app using React Native. <br/>
       <a href="https://github.com/Snome-Snow-Home/Snome">Snome on GitHub</a>
       <h2>ScrollMaster</h2>
-      Live Link: <a href="https://scrollmaster-image-gallery.vercel.app/" target="_blank" rel="noreferrer">ScrollMaster on Vercel</a>
+        A simple image search tool built on Unsplash, Next.js, and React.
+        <br/>
+      <a href="https://scrollmaster-image-gallery.vercel.app/" target="_blank" rel="noreferrer">ScrollMaster on Vercel</a>
 
       <h2>Vanity Number Generator</h2>
-      Call: <a href="tel:2284473585">(228) 447-3585</a>
+      A hotline that will tell the caller any vanity phone numbers that can be created using their phone number. <br/>
+      <a href="tel:2284473585">(228) 447-3585</a>
 
     </div>
   )
